73a3152c6e80b60ff5ab3f44a3007a3092d7e620
Alex Martyshko <martyshkoal@gmail.com>
2022-11-08 13:42:47 +0200

    test commit

---
+++ new.js
@@ -0,0 +1,399 @@
+const _data = [
+  {
+    name: 'Image 1',
+    description: 'nissan c 12',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShpwwsYOgIkskj3CDiuO2lXj6Di8uRU2NUtQ&usqp=CAU',
+  },
+  {
+    name: 'Image 2',
+    description: 'nissans 240sx',
+    src: 'https://mashinki.in.ua/image/cache/catalog/Hot%20Wheels/hot%20wheels%20Nissan%20Fairlady%20Z%20Fast%20Rewind%20blister-1000x1000.jpeg',
+  },
+  {
+    name: 'Image 3',
+    description: 'nissan skyline 2000',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr6f8KRct3AzEGqrCqslpk0oRcxcLLiZDVV144-lA-uoE0TfO4Hw7HbMw8maaJBPJrmwY&usqp=CAU',
+  },
+  {
+    name: 'Image 4',
+    description: 'nisssan skyline r32',
+    src: 'https://i.ebayimg.com/images/g/QBUAAOSwz1NgoC7E/s-l1600.jpg',
+  },
+  {
+    name: 'Image 5',
+    description: 'nissan skyline r 34',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRUAAd5mYWPB6ncJf5rze-YVU6k53lkMEsRXFZo20-MjgEmG2UII0r42LMjHBuI0Gkq24&usqp=CAU',
+  },
+  {
+    name: 'Image 6',
+    description: 'nissan skyline r 33',
+    src: 'https://a.allegroimg.com/s1024/0ca279/a70262d14da5a27e7c167bafb0ae',
+  },
+  {
+    name: 'Image 7',
+    description: 'nissan skyline KDR 30',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmzFm-iSP7ZWBKD965RJMexuMaLNUd6MpTNFAyBBOsuEmSSUw5_IvBlzMND1PULARSkbE&usqp=CAU',
+  },
+  {
+    name: 'Image 8',
+    description: 'aston martin db5',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRH30rHRppD0o78L9mGPsNqvGFt8crArIfGw&usqp=CAU',
+  },
+  {
+    name: 'Image 9',
+    description: 'lamborghini aventador coupe',
+    src: 'https://avatars.mds.yandex.net/get-mpic/5234821/img_id504070836341685015.jpeg/orig',
+  },
+  {
+    name: 'Image 10',
+    description: 'toyota ae86 trueno',
+    src: 'https://productimages.hepsiburada.net/s/125/375/110000075476608.jpg',
+  },
+  {
+    name: 'Image 11',
+    description: 'bmw M3 E30',
+    src: 'https://cdn.dsmcdn.com/mnresize/500/-/ty234/product/media/images/20211110/1/172624832/300618009/1/1_org.jpg',
+  },
+  {
+    name: 'Image 12',
+    description: 'bmw M3 E36',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRINSqa1is7BBetRRg0cV2P_isCU4miboGPWhrTUd6h3V0e6s0sVLZauS-XKonxGBvZ5Ko&usqp=CAU',
+  },
+  {
+    name: 'Image 13',
+    description: 'chevrolet shevelle SS 70',
+    src: 'https://korob.pl/pol_pl_HOT-WHEELS-PREMIUM-FAST-FURIOUS-70-Chevelle-GRL81-371700_1.jpg',
+  },
+  {
+    name: 'Image 14',
+    description: 'plymouth GTX 71',
+    src: 'https://a.allegroimg.com/original/118136/02db00504c6e9f885a40b870cb64/HOT-WHEELS-FAST-FURIOUS-ZESTAW-FULL-FORCE-PREMIUM-Marka-Hot-Wheels',
+  },
+  {
+    name: 'Image 15',
+    description: 'pontiac firebird custom 67',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-cwaBoDraEpxaNyBtleq4QEF6YlEphmUcfNlMB780teH0DiwbSyj_gg7T8YOqxVP9CQo&usqp=CAU',
+  },
+  {
+    name: 'Image 16',
+    description: 'plymouth barracuda hemi',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRafAiBa90IPszaxM8R1BY6aY8P81kKXI5DkPMRhOY1PK9pGN4X7VQ6G_lt-xt1FhOL348&usqp=CAU',
+  },
+  {
+    name: 'Image 17',
+    description: 'toyota celica gt-four 94',
+    src: 'https://media.entertainmentearth.com/assets/images/4e30c107c42741fcbb604f6d0d52cdd5xl.jpg',
+  },
+  {
+    name: 'Image 18',
+    description: 'nissan 300zx',
+    src: 'https://a.allegroimg.com/original/111a31/ba56d69f482099417ccd717771fd/HOT-WHEELS-PREMIUM-Car-Culture-Nissan-300ZX-Twin-T',
+  },
+  {
+    name: 'Image 19',
+    description: 'honda prelude 98',
+    src: 'https://a.allegroimg.com/original/11a355/e6984ace4f38b46ccc9628f5fdf7/HOT-WHEELS-PREMIUM-Car-Culture-98-Honda-Prelude',
+  },
+  {
+    name: 'Image 20',
+    description: 'honda nsx type-r 2003',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStlc0OrZd55HmEfQ0HBpEASFcWy0J_1zh5rSCcDOjol22C43ZxoxBCe0sv83T6JF57nKk&usqp=CAU',
+  },
+  {
+    name: 'Image 21',
+    description: 'acura nsx 2017',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrUnkEP2b9B1hiaufNAp8G0ukBGCqQ1pZLqHC5NDqTaGcjzBec2R7M5L5-t6ACthV7JFg&usqp=CAU',
+  },
+  {
+    name: 'Image 22',
+    description: 'chevrolet nova 66',
+    src: 'https://carmod.pl/5579-large_default/hot-wheels-164-fast-furious-66-chevy-nova.jpg',
+  },
+  {
+    name: 'Image 23',
+    description: 'chevrolet impala 61',
+    src: 'https://globaldiecastdirect.com/52508-thickbox_default/hot-wheels-fast-furious-1961-chevrolet-impala.jpg',
+  },
+  {
+    name: 'Image 24',
+    description: 'chevrolet nova panel 64',
+    src: 'https://bucket.carmodel.com/images/cm-lg/147014.jpg',
+  },
+  {
+    name: 'Image 25',
+    description: 'datsun sunny truck 75',
+    src: 'https://cdn1.ozone.ru/s3/multimedia-i/6017276526.jpg',
+  },
+  {
+    name: 'Image 26',
+    description: 'volskwagen caddy',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPcIGqZuR47D-Vuot_Z5rPobgncgwFx2mHw3IJyGMOJdbEN4nLE-b_uhvSENwbuJwdmDo&usqp=CAU',
+  },
+  {
+    name: 'Image 27',
+    description: 'datsun 620',
+    src: 'https://img.panama.ua/v/v1/v14ffddpcsqz.jpg',
+  },
+  {
+    name: 'Image 28',
+    description: 'mazda cosmo sport 1968',
+    src: 'https://cdn1.ozone.ru/s3/multimedia-j/6201087883.jpg',
+  },
+  {
+    name: 'Image 29',
+    description: 'toyota 2000 t',
+    src: 'https://images.prom.ua/1728404727_w700_h500_kollektsionnaya-mashinka-hot.jpg',
+  },
+  {
+    name: 'Image 30',
+    description: 'maxda rx-7 95',
+    src: 'https://images.izi.ua/157510267',
+  },
+  {
+    name: 'Image 31',
+    description: 'honda s 2000',
+    src: 'https://images.prom.ua/3467879925_w640_h640_kollektsionnaya-model-hot.jpg',
+  },
+  {
+    name: 'Image 32',
+    description: 'subaru brz',
+    src: 'https://cdn.dsmcdn.com/ty177/product/media/images/20210918/18/132579075/245278092/0/0_org_zoom.jpg',
+  },
+  {
+    name: 'Image 33',
+    description: 'honda civic ef 90',
+    src: 'https://www.funtastik.by/upload/iblock/3dd/3ddd4a2eb5f5369466080ead461444d1.jpg',
+  },
+
+
+]
+const INIFINY_TYPE = {
+  value: 'infinityScroll',
+  name: 'Infinity',
+  perPage: 12,
+}
+const REFULAR_TYPE = {
+  value: 'regular',
+  name: 'Regular',
+  perPage: 9,
+}
+const PAGINATION_TYPES = [
+  INIFINY_TYPE,
+  REFULAR_TYPE,
+]
+let paginationType = PAGINATION_TYPES[0].value;
+let currentPage = 1;
+const data = [];
+for (var i = 0; i < 500; i++) {
+  data.push({
+    ..._data[i % (_data.length - 1)],
+    title: i + 1,
+    name: i,
+  });
+}
+
+function createContainer() {
+  const divEl = document.createElement('div');
+  document.body.appendChild(divEl);
+  divEl.className = 'container';
+}
+
+function renderPage() {
+  let divEl = document.querySelector('.container');
+  const config = PAGINATION_TYPES.find(function (paginationConfig) {
+    return paginationConfig.value === paginationType;
+  });
+  const maxPage = Math.ceil(data.length / config.perPage);
+  let startInd = config.perPage * (currentPage - 1);
+  let lastIndex = startInd + config.perPage;
+  if (currentPage === maxPage) {
+    lastIndex = data.length;
+  }
+
+  for (let i = startInd; i < lastIndex; i++) {
+    let imgEl = document.createElement('img');
+    divEl.appendChild(imgEl);
+    imgEl.src = data[i].src;
+    imgEl.title = `${data[i].description} (${data[i].title})`
+    if ( data[i].src > data.length) {
+      break;
+    }
+  }
+
+  console.log(startInd);
+  console.log(lastIndex);
+  if (paginationType === REFULAR_TYPE.value) {
+    createPagination();
+  }
+}
+
+function createPagination() {
+   const config = PAGINATION_TYPES.find(function (paginationConfig) {
+     return paginationConfig.value === paginationType;
+   });
+  const maxPage = Math.ceil(data.length / config.perPage);
+  let prevPaginate = document.querySelector('.pagination')
+  if(prevPaginate) {
+    prevPaginate.remove()
+  }
+  const paginate = document.createElement('div');
+  document.body.appendChild(paginate);
+  paginate.className = 'pagination';
+
+  const prev = document.createElement('button');
+
+  paginate.appendChild(prev);
+  prev.textContent = 'prev';
+  prev.className = 'prev'
+
+  prev.addEventListener('click', function () {
+    let divEl = document.querySelector('.container');
+    divEl.innerHTML = '';
+    renderPage(--currentPage);
+  });
+  if (currentPage === 1) {
+    prev.disabled = true;
+  } else {
+    prev.disabled = false;
+  }
+
+  if (currentPage === 1) {
+    start = 1;
+    end =Math.min(start + 2, maxPage - 1);
+
+  } else if (currentPage !== 1 && currentPage !== maxPage) {
+    start = currentPage - 1;
+    end = currentPage + 1;
+  } else {
+    start = Math.max(currentPage - 2, 1);
+    end = maxPage;
+  }
+
+  for (let i = start; i <= end; i++) {
+    let buttonEl = document.createElement('button');
+    paginate.appendChild(buttonEl);
+    buttonEl.textContent = i;
+    buttonEl.className = 'btn';
+
+    buttonEl.addEventListener('click', () => {
+      renderPage(i);
+    })
+  }
+
+  const next = document.createElement('button');
+  paginate.appendChild(next);
+  next.textContent = 'next';
+  next.className = 'next';
+  next.addEventListener('click', function () {
+    let divEl = document.querySelector('.container');
+    divEl.innerHTML = '';
+    renderPage(++currentPage);
+  });
+
+  if (currentPage === maxPage) {
+    next.disabled = true;
+  }else {
+    next.disabled = false;
+  }
+}
+
+function createPaginationtypeSelector() {
+
+  const selectEl = document.createElement('select');
+  selectEl.className = 'select';
+  document.body.appendChild(selectEl);
+
+  for (let i = 0; i < PAGINATION_TYPES.length; i++) {
+    const optionEl = document.createElement('option');
+    selectEl.appendChild(optionEl);
+    optionEl.textContent = PAGINATION_TYPES[i].name;
+    optionEl.value = PAGINATION_TYPES[i].value;
+  }
+
+  selectEl.addEventListener('change', function () {
+    const prevPagination = document.querySelector('.pagination');
+    if (prevPagination) {
+      prevPagination.remove();
+    }
+
+    if (selectEl.value === REFULAR_TYPE.value) {
+      window.removeEventListener("scroll", infinitScrolListener,)
+    }
+
+    const config = PAGINATION_TYPES.find(function (paginationConfig) {
+      return paginationConfig.value === paginationType;
+    })
+
+    let firstIndexImg = (currentPage - 1) * config.perPage;
+    let lastIndexImg = (firstIndexImg + config.perPage);
+
+    const newconfig = PAGINATION_TYPES.find(function (paginationConfig) {
+      return paginationConfig.value === selectEl.value;
+
+    });
+
+     let newCurrentPage = (firstIndexImg + 1) / newconfig.perPage;
+
+     currentPage = Math.ceil(newCurrentPage);
+
+    paginationType = selectEl.value
+    choosePaginationType();
+    //renderPage();
+    console.log(currentPage);
+  })
+}
+
+function infinitScrolListener() {
+  const config = PAGINATION_TYPES.find(function (paginationConfig) {
+    return paginationConfig.value === paginationType;
+  });
+  const maxPage = Math.ceil(data.length / config.perPage);
+
+  if (window.scrollY + window.innerHeight >= document.body.clientHeight) {
+    currentPage++
+    renderPage();
+  }  if (currentPage === maxPage) {
+    window.removeEventListener("scroll",infinitScrolListener );
+  }
+}
+
+function subcribbeToScrollEvent() {
+
+  const config = PAGINATION_TYPES.find(function (paginationConfig) {
+
+    return paginationConfig.value === paginationType;
+  });
+  const maxPage = Math.ceil(data.length / config.perPage);
+
+  window.addEventListener('scroll', infinitScrolListener);
+  if (currentPage === maxPage) {
+    window.removeEventListener("scroll",infinitScrolListener );
+  }
+}
+
+function regularType() {
+  let divEl = document.querySelector('.container');
+  divEl.innerHTML = '';
+
+  renderPage();
+}
+
+function choosePaginationType() {
+
+  if (paginationType === REFULAR_TYPE.value) {
+    regularType();
+  } else if (paginationType === INIFINY_TYPE.value) {
+    subcribbeToScrollEvent();
+
+    let prevPaginate = document.querySelector('.pagination')
+    if(prevPaginate) {
+      prevPaginate.remove()
+    }
+  }
+}
+
+createContainer();
+renderPage();
+choosePaginationType();
+createPaginationtypeSelector();
+
